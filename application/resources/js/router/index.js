60ac40625a28b2f0798d109fe891d735defeaa1e
vsereda <vladimir45822@gmail.com>
2023-02-02 19:47:39 +0200

    fix routing

--- application/resources/js/router/index.js
+++ application/resources/js/router/index.js
@@ -1,7 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 import Home from "../pages/Home.vue";
-import Article from "../pages/UpdateArticle.vue";
+import UpdateArticle from "../pages/UpdateArticle.vue";
+import NewArticle from "../pages/NewArticle.vue";
 import ArticleSearch from "../pages/ArticleSearch.vue";
 
 const routes = [
@@ -12,13 +13,13 @@ const routes = [
     },
     {
         path: '/articles/create',
-        component: Article,
+        component: NewArticle,
         name: 'article.create',
     },
     {
         path: '/articles/:id(\\d+)',
-        component: Article,
-        name: 'article',
+        component: UpdateArticle,
+        name: 'article.edit',
     },
     {
         path: '/articles/search',
