e9e68d556c1401b3b9f68f109a1419d9c4ef2482
vsereda <vladimir45822@gmail.com>
2023-02-03 11:53:17 +0200

    refactoring backend routes

--- application/app/Http/Controllers/ArticleController.php
+++ application/app/Http/Controllers/ArticleController.php
@@ -8,32 +8,11 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Article::orderBy('id', 'desc')->select('id', 'title', 'body')->paginate(5);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $articleId = Article::insertGetId([
@@ -44,46 +23,16 @@ class ArticleController extends Controller
         return ['success' => $articleId > 0, 'article_id' => $articleId];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Article  $article
-     * @return array|\Illuminate\Http\Response
-     */
     public function show(Article $article)
     {
         return $article->only(['id', 'title', 'body']);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Article $article)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Article  $article
-     * @return array|\Illuminate\Http\Response
-     */
     public function update(Request $request, Article $article)
     {
         return ['success' => $article->update($request->only('title', 'body'))];
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Article $article)
     {
         return ['success' => $article->delete()];
